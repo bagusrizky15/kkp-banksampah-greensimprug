faff1e8e07722cc5d0f27e5dde2f9ca584f70e17
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-07-22 20:07:38 +0700

    banksampah

--- connection/connection.php
+++ connection/connection.php
@@ -1,5 +1,5 @@
 <?php
-    $db = new PDO('mysql:host=localhost;dbname=db_banksampah;charset=utf8mb4', 'root', '');
+    $db = new PDO('mysql:host=localhost;dbname=db_banksampah;charset=utf8mb4', 'root', 'root');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 ?>
